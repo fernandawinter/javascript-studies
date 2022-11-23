0844037859373d9c4c4c2f5ada0fe04dda0771a9
Fernanda Winter <fernanda.winter@hotmail.com>
2022-11-23 11:42:45 -0300

    adding informatios about conditional statements

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -9,3 +9,22 @@
 // A switch statement can be used to simplify the process of writing multiple else if statements. The break keyword stops the remaining cases from being checked and executed in a switch statement.
 
 // https://www.codecademy.com/learn/introduction-to-javascript/modules/learn-javascript-control-flow/cheatsheet
+
+
+
+// What are Conditional Statements?
+// In life, we make decisions based on circumstances. Think of an everyday decision as mundane as falling asleep — if we are tired, we go to bed, otherwise, we wake up and start our day.
+
+// These if-else decisions can be modeled in code by creating conditional statements. A conditional statement checks a specific condition(s) and performs a task based on the condition(s).
+
+// In this lesson, we will explore how programs make decisions by evaluating conditions and introduce logic into our code!
+
+// We’ll be covering the following concepts:
+
+// if, else if, and else statements
+// comparison operators
+// logical operators
+// truthy vs falsy values
+// ternary operators
+// switch statement
+// So if you’re ready to learn these concepts go to the next lesson— else, read over the concepts, observe the diagram, and prepare yourself for this lesson!
