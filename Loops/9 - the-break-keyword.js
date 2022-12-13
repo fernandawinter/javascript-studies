ffd3e50d539c71ebfd2430e5bc03b420d58206fe
Fernanda Winter <fernanda.winter@hotmail.com>
2022-12-13 14:29:02 -0300

    adding information about the break keyword

--- Loops/9 - the-break-keyword.js
+++ Loops/9 - the-break-keyword.js
@@ -28,3 +28,17 @@
 // After the for loop, log the string "And if you don't know, now you know." to the console. Note: since there’s a single quote character, ', in our string, we can use double quotes around the string to make sure character prints.
 
 // Add a break inside your loop’s block that breaks out of the loop if the element at the current index in the rapperArray is 'Notorious B.I.G.'.
+
+
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+    console.log(rapperArray[i]);
+    if (rapperArray[i] === 'Notorious B.I.G.') {
+        break;
+    }
+}
+
+console.log("And if you don't know, now you know.");
