bfc93da5831cf4665710e7803b7fcc7fc7d6e384
Fernanda Winter <fernanda.winter@hotmail.com>
2022-12-25 22:16:09 -0300

    adding information about iterators - functions as data

--- Iterators/1 - Higher order functions/1 - introduction.js
+++ Iterators/1 - Higher order functions/1 - introduction.js
@@ -8,3 +8,6 @@
 // In this lesson, we’ll learn how to use “abstraction” in programming by writing functions.In addition to allowing us to reuse our code, functions help to make clear, readable programs.If you encounter countToThree() in a program, you might be able to quickly guess what the function does without having to stop and read the function’s body.
 
 //     We’re also going to learn about a way to add another level of abstraction to our programming: higher - order functions.Higher - order functions are functions that accept other functions as arguments and / or return functions as output.This enables us to build abstractions on other abstractions, just like “We hosted a birthday party” is an abstraction that may build on the abstraction “We made a cake.”
+
+
+// https://www.codecademy.com/learn/introduction-to-javascript/modules/learn-javascript-iterators/cheatsheet
