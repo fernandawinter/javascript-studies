4adcc32d7a47e8784e956ee07b7cdafa211a2e1a
Fernanda Winter <fernanda.winter@hotmail.com>
2022-12-27 21:37:24 -0300

    adding information about iterators - introduction

--- Iterators/1 - Higher order functions/1 - introduction.js
+++ Iterators/1 - Higher order functions/1 - introduction.js
@@ -11,3 +11,16 @@
 
 
 // https://www.codecademy.com/learn/introduction-to-javascript/modules/learn-javascript-iterators/cheatsheet
+
+
+// Let’s review what we learned in this lesson:
+
+// Abstraction allows us to write complicated code in a way that’s easy to reuse, debug, and understand for human readers.
+
+// We can work with functions the same way we work with any other type of data, including reassigning them to new variables.
+
+// JavaScript functions are first-class objects, so they have properties and methods like any other object.
+
+// Functions can be passed into other functions as parameters.
+
+// A higher-order function is a function that either accepts functions as parameters, returns a function, or both.
