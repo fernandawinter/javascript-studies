a41c2c227bf226d91945485224d8a388f43b80a1
Fernanda Winter <fernanda.winter@hotmail.com>
2023-01-02 18:03:25 -0300

    adding more information about iterators - filter method

--- Iterators/2 - Iterators/4 - the-filter-method.js
+++ Iterators/2 - Iterators/4 - the-filter-method.js
@@ -14,3 +14,27 @@
 // console.log(words); // Output: ['chair', 'music', 'pillow', 'brick', 'pen', 'door']; 
 // console.log(shortWords); // Output: ['chair', 'music', 'brick', 'pen', 'door']
 // Observe how words was not mutated, i.e.changed, and shortWords is a new array.
+
+
+
+
+// Call the .filter() method on randomNumbers to return values that are less than 250. Save them to a new array called smallNumbers, declared with const.
+
+// Now let’s work with an array of strings. Invoke .filter() on the favoriteWords array to return elements that have more than 7 characters. Save the returned array to a const variable named longFavoriteWords.
+
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250;
+})
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+})
