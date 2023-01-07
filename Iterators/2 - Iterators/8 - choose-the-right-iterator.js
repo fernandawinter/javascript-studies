ec0d470182487ce7376e40a375aaf30a085e680a
Fernanda Winter <fernanda.winter@hotmail.com>
2023-01-07 11:58:03 -0300

    adding information about iterators - choose the right iterator

---
+++ Iterators/2 - Iterators/8 - choose-the-right-iterator.js
@@ -0,0 +1,4 @@
+// Choose the Right Iterator
+// There are many iteration methods you can choose. In addition to learning the correct syntax for the use of iteration methods, it is also important to learn how to choose the correct method for different scenarios. The exercises below will give you the opportunity to do just that!
+
+// You’ll see errors pop up in the terminal as you work through this exercise, but by the end the errors will be gone!
