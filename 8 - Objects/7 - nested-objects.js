724a824d6cdf4da23bd12a2479f82431fe25ac60
Fernanda Winter <fernanda.winter@hotmail.com>
2023-01-19 18:44:42 -0300

    adding information about objects - nested objects

--- 8 - Objects/7 - nested-objects.js
+++ 8 - Objects/7 - nested-objects.js
@@ -38,3 +38,46 @@
 // First the computer evaluates spaceship.nanoelectronics, which results in an object containing the back - up and computer objects.
 // We accessed the back - up object by appending['back-up'].
 // The back - up object has a battery property, accessed with .battery which returned the value stored there: 'Lithium'
+
+
+
+// Create a variable capFave and assign the captain‘s favorite food (the element in the 0th index of her 'favorite foods' array) to it. Make sure to use bracket and dot notation to get the value of the food through nested access (don’t just copy the value into the variable!)
+
+// Right now the passengers property has a value of null. Instead, assign as its value an array of objects. These objects should represent the spaceship‘s passengers as individual objects. Make at least one passenger object in the array that has at least one key-value pair on it.
+
+// Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). Make sure to use bracket and dot notation to get the passenger object through nested access (don’t just copy the object into the variable!)
+
+
+let spaceship = {
+    passengers: [{ name: 'Space Dog' }],
+    telescope: {
+        yearBuilt: 2018,
+        model: "91031-XLT",
+        focalLength: 2032
+    },
+    crew: {
+        captain: {
+            name: 'Sandra',
+            degree: 'Computer Engineering',
+            encourageTeam() { console.log('We got this!') },
+            'favorite foods': ['cookies', 'cakes', 'candy', 'spinach']
+        }
+    },
+    engine: {
+        model: "Nimbus2000"
+    },
+    nanoelectronics: {
+        computer: {
+            terabytes: 100,
+            monitors: "HD"
+        },
+        'back-up': {
+            battery: "Lithium",
+            terabytes: 50
+        }
+    }
+};
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+
+let firstPassenger = spaceship.passengers[0];
