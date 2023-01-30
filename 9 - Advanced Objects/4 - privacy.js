566fe5216d77a73d49f4178fb62afe7870c689eb
Fernanda Winter <fernanda.winter@hotmail.com>
2023-01-30 18:14:17 -0300

    adding information about advanced objects - privacy

--- 9 - Advanced Objects/4 - privacy.js
+++ 9 - Advanced Objects/4 - privacy.js
@@ -12,3 +12,25 @@
 
 // bankAccount._amount = 1000000;
 // In later exercises, we’ll cover the use of methods called getters and setters.Both methods are used to respect the intention of properties prepended, or began, with _.Getters can return the value of internal properties and setters can safely reassign property values.For now, let’s see what happens if we can change properties that don’t have setters or getters.
+
+
+
+
+// Below the robot object, reassign the _energyLevel property to 'high'.
+
+// Now take a look at the new recharge method in robot. .recharge() will add 30 to _energyLevel.
+// What will happen now that _energyLevel isn’t a number ?
+//     Call.recharge() on robot to find out.
+// Notice that a funky string is printed to the console! This is known as a side - effect of type - coercion.No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side - effects when mutating objects and their properties.
+
+
+const robot = {
+    _energyLevel: 100,
+    recharge() {
+        this._energyLevel += 30;
+        console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+    }
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
