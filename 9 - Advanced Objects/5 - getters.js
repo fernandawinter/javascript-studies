70353b2ebc8aa35421a9c6b96d55a5f1733649b2
Fernanda Winter <fernanda.winter@hotmail.com>
2023-02-02 00:19:56 -0300

    adding information about advanced objects - getters

--- 9 - Advanced Objects/5 - getters.js
+++ 9 - Advanced Objects/5 - getters.js
@@ -44,3 +44,17 @@
 
 // Log the result of calling the getter method energyLevel on robot to the console.
 // Notice that the method will return a formatted response rather than just accessing a property!
+
+const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel() {
+        if (typeof this._energyLevel === 'number') {
+            return 'My current energy level is ' + this._energyLevel
+        } else {
+            return "System malfunction: cannot retrieve energy level"
+        }
+    }
+};
+
+console.log(robot.energyLevel);
